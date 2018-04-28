4021e660e1f459c4161003c850441cbf8046aaad
Sheldon <luporoee@gmail.com>
2018-04-28 21:20:58 +0300

    Create config.php

---
+++ captures/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'secure_key' => 'your_password',
+
+    'output_url' => 'http://yourdomain.com/captures/',
+
+    'redirect_url' => 'http://yourdomain.com/captures/',
+
+    'allowed_ips' => array(),
+
+    'page_title' => 'page_title',
+
+    'heading_text' => 'heading_text',
+);
