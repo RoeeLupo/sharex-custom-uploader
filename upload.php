7030faea37b9c2f46b8e72c3ce79d1fe1bd0820f
Sheldon <luporoee@gmail.com>
2018-04-28 21:19:25 +0300

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,26 @@
+<?php
+$config = include('captures/config.php');
+
+$key = $config['secure_key'];
+$uploadhost = $config['output_url'];
+$redirect = $config['redirect_url'];
+
+if ($_SERVER["REQUEST_URI"] == "/robot.txt") { die("User-agent: *\nDisallow: /"); }
+ 
+if (isset($_POST['key'])) {
+    if ($_POST['key'] == $key) {
+        $parts = explode(".", $_FILES["d"]["name"]);
+        $target = getcwd() . "/captures/" . $_POST['name'] . "." . end($parts);
+        if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
+            $target_parts = explode("/captures/", $target);
+            echo $uploadhost . end($target_parts);
+        } else {
+            echo "Sorry, there was a problem uploading your file. (Ensure your directory has 777 permissions)";
+        }
+    } else {
+        header('Location: '.$redirect);
+    }
+} else {
+    header('Location: '.$redirect);
+}
+?>
